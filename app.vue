3c1e8a61864e7715d48968200f6f5d13b0d70a79
Sourabh Shukla <34943727+ssoura@users.noreply.github.com>
2022-10-24 15:13:35 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,31 @@
+<script setup>
+const darkMode = ref(true)
+</script>
+
+<template>
+  <div :class="{ 'dark': darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!-- left -->
+          <div class="md:block xs:col-span-1 xl:col-span-2">
+            <div class="stickey top-0"></div>
+            <SidebarLeft />
+          </div>
+          <!-- main -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <h1>Main</h1>
+          </main>
+
+          <!-- right -->
+          <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              <SidebarRight />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
